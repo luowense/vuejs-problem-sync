539adcef253641fccd2bdd5c4bc2a790051862c1
luowense <laurent.roger.segura@gmail.com>
2020-11-26 18:45:23 +0100

    the problem

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <child v-bind:title="title" v-on:update-title="title=$event"></child>
+    <child :title.sync="title"></child>
+    <child v-bind:title="title" v-on:update-title="title=$event"></child>
+    <child v-bind:title="title" v-on:update-title="title=$event"></child>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Child from "./components/Child.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Child,
+  },
+  data(){
+    return {
+      title: 'myTitle'
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Child.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <h2>{{ title }}</h2>
+  <input v-on:input="$emit('update-title', $event.target.value)" v-bind:value="title" type="text"/>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Child',
+    props: ['title']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
